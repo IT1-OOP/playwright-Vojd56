b27a8da1fbe9c5fe044baa8eab58c99af39b10f2
Vojtěch Bláha <90373255+Vojd56@users.noreply.github.com>
2025-04-03 13:11:42 +0200

    neasine

--- tests/lokator_test.py
+++ tests/lokator_test.py
@@ -1,8 +1,13 @@
-from playwright.sync_api import sync_playwright
+import os
+from playwright.sync_api import sync_playwright, expect
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("file://" + "index.html")  # Načte lokální HTML soubor
-    print(page.title())  # Vypíše titulek stránky
-    browser.close()
+def test_page_title():
+    cesta = os.path.abspath("index.html")
+
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(f"file:///{cesta}")  # Načte lokální HTML soubor
+        expect(page.locator('h1').first).to_be_visible()
+        browser.close()
